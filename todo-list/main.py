d952237dc1d78662f23f86024925ae8c095c213b
dimka90 <dimkayilrit@gmail.com>
2025-10-02 06:03:47 +0100

    patch: added logic for updating partial content of a resources

--- todo-list/main.py
+++ todo-list/main.py
@@ -4,7 +4,7 @@ from datetime  import datetime
 from typing import List, Optional
 app=FastAPI()
 
-task_id = 1
+next_id = 1
 
 class TaskRequest(BaseModel):
    title: str
@@ -18,14 +18,17 @@ class TaskResponse(BaseModel):
    created_at: datetime
    updated_at: datetime
 
+class  TaskUpdate(BaseModel):
+   title: Optional[str] = None
+   description: Optional[str] =None
 
 tasks_db : List[TaskResponse]= []
 # POST   /tasks              - Create new task
 # GET    /tasks              - Get all tasks
-# GET    /tasks/{task_id}    - Get specific task
-# PUT    /tasks/{task_id}    - Update task
-# DELETE /tasks/{task_id}    - Delete task
-# PATCH  /tasks/{task_id}/complete - Toggle completion
+# GET    /tasks/{next_id}    - Get specific task
+# PUT    /tasks/{next_id}    - Update task
+# DELETE /tasks/{next_id}    - Delete task
+# PATCH  /tasks/{next_id}/complete - Toggle completion
 
 
 def find_task_by_id(id: int) -> TaskResponse| None:
@@ -42,10 +45,12 @@ def find_task_by_title(title: str ) -> TaskResponse| None:
          return task
    return None
 
+def get_current_time() -> datetime:
+   return datetime.utcnow()
 
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-   task = find_task_by_id(task_id)
+@app.get("/tasks/{next_id}")
+def get_task(next_id: int):
+   task = find_task_by_id(next_id)
    if task is None:
       raise HTTPException(
          status_code=status.HTTP_404_NOT_FOUND,
@@ -60,26 +65,25 @@ def get_task():
 
 @app.post("/tasks", status_code=201)
 def create_task(task: TaskRequest):
-   global task_id
+   global next_id
    # to_do() -> validation 
    
    new_task = TaskResponse(
-      id=task_id,
+      id=next_id,
       title= task.title,
       description= task.description,
       is_completed=False,
       created_at= datetime.utcnow(),
       updated_at=datetime.utcnow()
    )
-   task_id += 1
+   next_id += 1
    tasks_db.append(new_task)
 
    return  new_task
 
-
-@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(task_id: int):
-   task = find_task_by_id(task_id)
+@app.delete("/tasks/{next_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(next_id: int):
+   task = find_task_by_id(next_id)
    if task is None:
       raise HTTPException(
          status_code=status.HTTP_404_NOT_FOUND,
@@ -88,20 +92,31 @@ def delete_task(task_id: int):
    tasks_db.remove(task)
    return None
    
-# @app.put("/tasks/{task_id}")
-# def update_a_task(task_id:int , task: Task):
-#    print(task_id)
-#    if task_id > len(tasks_db):
-#       return {
-#         "success": False,
-#          "message" : "Invalid task Id"
-#       }
-#    tasks_db[task_id - 1] = task
-#    tasks_db[task_id-1].id = task_id
-#    return {
-#       "success": True,
-#       "data": tasks_db[task_id -1],
-#       "message": "All field updated successfully"
-#    }
+@app.put("/tasks/{next_id}", status_code=status.HTTP_206_PARTIAL_CONTENT)
+def update_a_task(next_id: int, new_task: TaskUpdate) -> TaskResponse:
+   task = find_task_by_id(next_id)
+   if task is None:
+      raise HTTPException(
+         status_code=status.HTTP_404_NOT_FOUND,
+         detail=" Invalid Id"
+      )
+   task.title = new_task.title
+   task.description = new_task.description
+   task.updated_at = get_current_time()
+   return task
 
- 
+@app.patch("/tasks/{next_id}", status_code=status.HTTP_206_PARTIAL_CONTENT)
+def update_a_task(next_id: int, new_task: TaskUpdate) -> TaskResponse:
+   
+   task_to_update = find_task_by_id(next_id)
+   if task_to_update is None:
+      raise HTTPException(
+         status_code=status.HTTP_404_NOT_FOUND,
+         detail=" Invalid Id"
+      )
+   if new_task.title is not None:
+      task_to_update.title = new_task.title
+   if new_task.description is not None:
+      task_to_update.description = new_task.description
+
+   return task_to_update
