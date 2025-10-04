adf44aded6f58d3a055540a36cea93c4e999d900
dimka90 <dimkayilrit@gmail.com>
2025-10-04 13:25:12 +0100

    jwt: used jwt token for encoding and sending user token

--- auth/main.py
+++ auth/main.py
@@ -1,21 +1,37 @@
 from fastapi import Depends, FastAPI, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from pydantic import BaseModel
 from typing import List, Annotated
+import os
+from dotenv import load_dotenv
+
+import jwt
+from pwdlib import PasswordHash
+load_dotenv()
 app = FastAPI()
 
-
+"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRATION_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer("token")
 user_next_id = 1
 
+password_hashed = PasswordHash.recommended()
+
+
+print(password_hashed)
 def generate_next_id():
     return user_next_id 
 
 def hashed_password(password: str) -> str:
-    return f"{password}+abc*"
+    return password_hashed.hash(password)
 
+def verify_password(hashed_password: str, plain_password: str):
+    return password_hashed.verify(hashed_password, plain_password)
 
+result = hashed_password("james")
+print(verify_password("james", result))
 class UserResquest(BaseModel):
     username: str
     password: str
@@ -48,6 +64,20 @@ def index():
         "Message": "Hello, World"
     }
 
+def generate_access_token( data: dict, expiry_delta: timedelta | None = None) -> str:
+    encode_data = data.copy()
+    if  expiry_delta:
+        expire = datetime.now(timezone.utc) + expiry_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    encode_data.update({
+        "exp": expire
+    })
+    encode_jwt = jwt.encode(encode_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encode_jwt
+
 @app.post("/api/users/", status_code=status.HTTP_201_CREATED)
 def create_user(user: UserResquest) -> UserResponse:
     global user_next_id
@@ -78,7 +108,7 @@ def create_user(user: UserResquest) -> UserResponse:
     
     return user_response
 
-@app.post("/token")
+@app.post("/api/token")
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_name = form_data.username
     user = find_user_by_username( user_name)
@@ -88,14 +118,20 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
             detail="user does not exist"
         )
     user_password = form_data.password
-    checked_user_password = check_password_match(user, user_password)
+
+    checked_user_password = verify_password(form_data.password, user.hashed_password)
     if  not checked_user_password:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect Password"
         )
     # ToDo
+    access_token = generate_access_token( data= {
+        "sub": user.username,
+    }, expiry_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MINUTES))
+
     return {
-        "access-token": user.username,
+        "access-token": access_token,
         "token-type": "bearer"
     }
+
